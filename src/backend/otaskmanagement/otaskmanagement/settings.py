99b1a451dc3d9396a1d4d5298785bfaac1f7b5ba
Duong Qui On <quyonduong@gmail.com>
2026-01-02 20:35:26 +0700

    reConfig CORS

--- src/backend/otaskmanagement/otaskmanagement/settings.py
+++ src/backend/otaskmanagement/otaskmanagement/settings.py
@@ -54,6 +54,7 @@ THIRD_PARTY_APPS = [
     "dj_rest_auth",
     "dj_rest_auth.registration",
     "rest_framework.authtoken",
+    "corsheaders",
 ]
 
 PROJECT_APPS = [
@@ -79,6 +80,7 @@ PROJECT_APP_LABELS = {app.split('.')[-1] for app in PROJECT_APPS}
 # Middleware
 # ------------------------------------------------------------------ #
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -88,6 +90,13 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+]
+
+CORS_ALLOWED_ALL_ORIGINS = True
 
 # ------------------------------------------------------------------ #
 # Auth / Allauth
